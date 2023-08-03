7f784b5fa3523f06d2a2adc9f3407eacb3ac7535
Hemapriya P <hemapriya.pandian@hurix.com>
2023-08-03 18:03:04 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./App.css";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 // import { MovieCard } from "./MovieCard";
 
@@ -8,6 +10,117 @@ import "./App.css";
 import MovieList from "./MovieCard";
 
 export default function App() {
+  const inital_movie_list = [
+    {
+      id: "99",
+      name: "Vikram",
+      poster:
+        "https://m.media-amazon.com/images/M/MV5BMmJhYTYxMGEtNjQ5NS00MWZiLWEwN2ItYjJmMWE2YTU1YWYxXkEyXkFqcGdeQXVyMTEzNzg0Mjkx._V1_.jpg",
+      rating: 8.4,
+      summary:
+        "Members of a black ops team must track and eliminate a gang of masked murderers.",
+      trailer: "https://www.youtube.com/embed/OKBMCL-frPU",
+    },
+    {
+      id: "100",
+      name: "RRR",
+      poster:
+        "https://englishtribuneimages.blob.core.windows.net/gallary-content/2021/6/Desk/2021_6$largeimg_977224513.JPG",
+      rating: 8.8,
+      summary:
+        "RRR is an upcoming Indian Telugu-language period action drama film directed by S. S. Rajamouli, and produced by D. V. V. Danayya of DVV Entertainments.",
+      trailer: "https://www.youtube.com/embed/f_vbAtFSEc0",
+    },
+    {
+      id: "101",
+      name: "Iron man 2",
+      poster:
+        "https://m.media-amazon.com/images/M/MV5BMTM0MDgwNjMyMl5BMl5BanBnXkFtZTcwNTg3NzAzMw@@._V1_FMjpg_UX1000_.jpg",
+      rating: 7,
+      summary:
+        "With the world now aware that he is Iron Man, billionaire inventor Tony Stark (Robert Downey Jr.) faces pressure from all sides to share his technology with the military. He is reluctant to divulge the secrets of his armored suit, fearing the information will fall into the wrong hands. With Pepper Potts (Gwyneth Paltrow) and Rhodes (Don Cheadle) by his side, Tony must forge new alliances and confront a powerful new enemy.",
+      trailer: "https://www.youtube.com/embed/wKtcmiifycU",
+    },
+    {
+      id: "102",
+      name: "No Country for Old Men",
+      poster:
+        "https://upload.wikimedia.org/wikipedia/en/8/8b/No_Country_for_Old_Men_poster.jpg",
+      rating: 8.1,
+      summary:
+        "A hunter's life takes a drastic turn when he discovers two million dollars while strolling through the aftermath of a drug deal. He is then pursued by a psychopathic killer who wants the money.",
+      trailer: "https://www.youtube.com/embed/38A__WT3-o0",
+    },
+    {
+      id: "103",
+      name: "Jai Bhim",
+      poster:
+        "https://m.media-amazon.com/images/M/MV5BY2Y5ZWMwZDgtZDQxYy00Mjk0LThhY2YtMmU1MTRmMjVhMjRiXkEyXkFqcGdeQXVyMTI1NDEyNTM5._V1_FMjpg_UX1000_.jpg",
+      summary:
+        "A tribal woman and a righteous lawyer battle in court to unravel the mystery around the disappearance of her husband, who was picked up the police on a false case",
+      rating: 8.8,
+      trailer: "https://www.youtube.com/embed/nnXpbTFrqXA",
+    },
+    {
+      id: "104",
+      name: "The Avengers",
+      rating: 8,
+      summary:
+        "Marvel's The Avengers (classified under the name Marvel Avengers\n Assemble in the United Kingdom and Ireland), or simply The Avengers, is\n a 2012 American superhero film based on the Marvel Comics superhero team\n of the same name.",
+      poster:
+        "https://terrigen-cdn-dev.marvel.com/content/prod/1x/avengersendgame_lob_crd_05.jpg",
+      trailer: "https://www.youtube.com/embed/eOrNdBpGMv8",
+    },
+    {
+      id: "105",
+      name: "Interstellar",
+      poster: "https://m.media-amazon.com/images/I/A1JVqNMI7UL._SL1500_.jpg",
+      rating: 8.6,
+      summary:
+        "When Earth becomes uninhabitable in the future, a farmer and ex-NASA\n pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team\n of researchers, to find a new planet for humans.",
+      trailer: "https://www.youtube.com/embed/zSWdZVtXT7E",
+    },
+    {
+      id: "106",
+      name: "Baahubali",
+      poster: "https://flxt.tmsimg.com/assets/p11546593_p_v10_af.jpg",
+      rating: 8,
+      summary:
+        "In the kingdom of Mahishmati, Shivudu falls in love with a young warrior woman. While trying to woo her, he learns about the conflict-ridden past of his family and his true legacy.",
+      trailer: "https://www.youtube.com/embed/sOEg_YZQsTI",
+    },
+    {
+      id: "107",
+      name: "Ratatouille",
+      poster:
+        "https://resizing.flixster.com/gL_JpWcD7sNHNYSwI1ff069Yyug=/ems.ZW1zLXByZC1hc3NldHMvbW92aWVzLzc4ZmJhZjZiLTEzNWMtNDIwOC1hYzU1LTgwZjE3ZjQzNTdiNy5qcGc=",
+      rating: 8,
+      summary:
+        "Remy, a rat, aspires to become a renowned French chef. However, he fails to realise that people despise rodents and will never enjoy a meal cooked by him.",
+      trailer: "https://www.youtube.com/embed/NgsQ8mVkN8w",
+    },
+    {
+      name: "PS2",
+      poster:
+        "https://m.media-amazon.com/images/M/MV5BYjFjMTQzY2EtZjQ5MC00NGUyLWJiYWMtZDI3MTQ1MGU4OGY2XkEyXkFqcGdeQXVyNDExMjcyMzA@._V1_.jpg",
+      summary:
+        "Ponniyin Selvan: I is an upcoming Indian Tamil-language epic period action film directed by Mani Ratnam, who co-wrote it with Elango Kumaravel and B. Jeyamohan",
+      rating: 8,
+      trailer: "https://www.youtube.com/embed/KsH2LA8pCjo",
+      id: "108",
+    },
+    {
+      name: "Thor: Ragnarok",
+      poster:
+        "https://m.media-amazon.com/images/M/MV5BMjMyNDkzMzI1OF5BMl5BanBnXkFtZTgwODcxODg5MjI@._V1_.jpg",
+      summary:
+        "When Earth becomes uninhabitable in the future, a farmer and ex-NASA\\n pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team\\n of researchers, to find a new planet for humans.",
+      rating: 8.8,
+      trailer: "https://youtu.be/NgsQ8mVkN8w",
+      id: "109",
+    },
+  ];
+  const [movielist, setmovielist] = useState(inital_movie_list);
   return (
     <div className="App">
       <h1>Welcome to React Router!</h1>
@@ -29,8 +142,11 @@ export default function App() {
 
       <Routes>
         <Route path="/home" element={<Home />} />
-        <Route path="/movies" element={<MovieList />} />
-        <Route path="/movies/:id" element={<MovieDetails />} />
+        <Route path="/movies" element={<MovieList movielist={movielist} />} />
+        <Route
+          path="/movies/:id"
+          element={<MovieDetails movielist={movielist} />}
+        />
 
         <Route path="*" element={<PageNotFound />} />
         {/* <Route path="/movies:id" element={<MovieList />} /> */}
@@ -43,9 +159,32 @@ function PageNotFound() {
   return <h1>404 error Page not found</h1>;
 }
 
-function MovieDetails() {
+function MovieDetails({ movielist }) {
+  const navigate = useNavigate();
   const { id } = useParams();
-  return <h1>Movie details -{id}</h1>;
+  const abc = movielist[id];
+  return (
+    <div className="Movie">
+      <iframe
+        width="1311"
+        height="649"
+        src="https://www.youtube.com/embed/OKBMCL-frPU"
+        title="VIKRAM - Official Trailer | Kamal Haasan | VijaySethupathi, FahadhFaasil | LokeshKanagaraj | Anirudh"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen
+      ></iframe>
+      <div className="spec">
+        <h2>
+          {abc.name}- {id}
+        </h2>
+        <p> ⭐ {abc.rating}</p>
+      </div>
+
+      <p>{abc.summary}</p>
+      <button onClick={() => navigate(-1)}>back</button>
+    </div>
+  );
 }
 
 function Home() {
